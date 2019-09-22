f57f3baec18d2a1c6b40f3dfa4c06beb0feb2467
Aridv <kuinatheauri@gmail.com>
2019-09-22 15:08:44 +0100

    First commit

---
+++ DeserializeAPIJson/DeserializeAPIJson/DeserializeAPIJson.vb
@@ -0,0 +1,22 @@
+﻿Imports Newtonsoft.Json.Linq
+
+Module DeserializeAPIJson
+
+    Sub Main()
+
+        Dim objectData = "{ '_name': 'Cristina', '_age': '26', '_country': 'ES', '_mobile': '123456789', '_email': 'git_test@example.com' }"
+        Dim arrayData = "[{ '_name': 'Cristina', '_age': '26', '_country': 'ES', '_mobile': '123456789', '_email': 'git_test@example.com' }]"
+
+        Dim jObj = JObject.Parse(objectData)
+        Dim jArr = JArray.Parse(arrayData)
+
+        Dim personResult = jObj.ToObject(Of Person)
+        Dim personListResult = jArr.ToObject(Of List(Of Person))
+
+        Console.WriteLine(personResult)
+        Console.WriteLine(personListResult)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ DeserializeAPIJson/DeserializeAPIJson/Models/Person.vb
@@ -0,0 +1,9 @@
+﻿Public Class Person
+
+    Public Property _name As String
+    Public Property _age As Integer
+    Public Property _country As String
+    Public Property _mobile As String
+    Public Property _email As String
+
+End Class
